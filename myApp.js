fe7f36269895e4d62f4feeaaa13be009f38f9143
thegoat309 <2049795@learn.vsb.bc.ca>
2026-05-12 20:15:28 +0000

    new chANGES

--- myApp.js
+++ myApp.js
@@ -1,7 +1,14 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
 app.use("/public", express.static(__dirname + '/public'));
+
+app.use(function(req,res, next) {
+    var message = req.method + " " + req.path + " - " + req.ip;
+    console.log(message);
+    next();
+});
 
 app.get("/", function(req, res) {
     res.sendfile(__dirname + "/views/index.html");
@@ -10,15 +17,17 @@ app.get("/", function(req, res) {
 app.get('/json', function(req,res){
     require('dotenv').config();
     if(process.env.MESSAGE_STYLE === "uppercase"){
-        res.json({"message": "Hello json"});
+        res.json({"message": "HELLO JSON"});
     } else {
         res.json({"message": "Hello json"})
     }
     });
 
-
-
-
+    app.get('/now', function(req, res, next){
+        req.time = new Date(). toString() 
+    } function(req, res) {
+        time: req.time;
+    });
 
 
 
